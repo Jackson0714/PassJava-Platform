9b2329602e225bbb4ab34b8768157e282aac748d
悟空 <>
2021-04-29 15:24:23 +0800

    update redis lock

--- passjava-question/src/main/java/com/jackson0714/passjava/question/service/impl/TypeServiceImpl.java
+++ passjava-question/src/main/java/com/jackson0714/passjava/question/service/impl/TypeServiceImpl.java
@@ -6,10 +6,13 @@ import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -99,17 +102,26 @@ public class TypeServiceImpl extends ServiceImpl<TypeDao, TypeEntity> implements
 
     @Override
     public List<TypeEntity> getTypeEntityListByRedisDistributedLock() throws InterruptedException {
-        // 1.先抢占锁
-        Boolean lock = redisTemplate.opsForValue().setIfAbsent("lock", "123");
+        // 1.生成唯一 id
+        String uuid = UUID.randomUUID().toString();
+        // 2. 抢占锁
+        Boolean lock = redisTemplate.opsForValue().setIfAbsent("lock", uuid, 10, TimeUnit.SECONDS);
         if(lock) {
-            // 2.抢占成功，执行业务
+            System.out.println("抢占成功：" + uuid);
+            // 3.抢占成功，执行业务
             List<TypeEntity> typeEntityListFromDb = getDataFromDB();
-            // 3.解锁
-            redisTemplate.delete("lock");
+
+            // 4.Lua 脚本解锁
+            String script = "if redis.call('get',KEYS[1]) == ARGV[1] then return redis.call('del',KEYS[1]) else return 0 end";
+            redisTemplate.execute(new DefaultRedisScript<Long>(script, Long.class), Arrays.asList("lock"), uuid);
+
+            System.out.println("解锁成功：" + uuid);
+
             return typeEntityListFromDb;
         } else {
+            System.out.println("抢占失败，等待锁释放");
             // 4.休眠一段时间
-            //sleep(100);
+            sleep(100);
             // 5.抢占失败，等待锁释放
             return getTypeEntityListByRedisDistributedLock();
         }
